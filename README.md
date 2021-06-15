39fb25a79883836df6090cfa341c3eb704e1e248
Lashaun <clarklas831@gmail.com>
2021-06-14 21:50:36 -0500

    add confirmations

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
-# Password Generator Starter Code
+## Tie function to button
+
+## Create password criteria
+- Reference: https://w3collective.com/random-password-generator-javascript/
+- https://stackoverflow.com/questions/1497481/javascript-password-generator
+
+## Allow for criteria selection
+
+## Allow user to choose length of password
+- Must be at least 8 characters
+- Give window prompts?
+
+## Allow user to include character types
+- Lowercase, uppercase, numerics, and/or special characters
+- Should show at least one selected character type
+
+## Generate the password based on the criteria
+- Fill in the HTML textarea
+
+## Display the password
---
+++ assets/js/script.js
@@ -0,0 +1,39 @@
+// Assignment code here
+
+
+// Get references to the #generate element
+var generateBtn = document.querySelector("#generate");
+
+// Write password to the #password input
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+
+  passwordText.value = password;
+
+}
+
+function generatePassword() {
+  let characterLengthPrompt = parseInt(window.prompt(`How many characters would you like your password to be?`));
+  console.log(characterLengthPrompt);
+  if (!characterLengthPrompt || typeof characterLengthPrompt === `string`) {
+    window.alert(`Please enter a number.`);
+    return generatePassword();
+  };
+  if (characterLengthPrompt < 8) {
+    window.alert(`password must be at least 8 characters!`);
+    return generatePassword();
+  };
+  let characterTypesConfirm = window.confirm(`Please choose what character types you'd like to include.`);
+  if (!characterTypesConfirm) {
+    window.alert(`You must choose at least one character type to include.`)
+  } 
+  let numberPrompt = window.confirm(`Would you like your password to include numbers?`);
+  let lowerCaseConfirm = window.confirm(`Would you like to include lower case letters?`);
+  let upperCaseConfirm = window.confirm(`Would you like to include upper case letters?`);
+  let specialCharacterConfirm = window.confirm(`Would you like to include special characters?`);
+
+  }
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
