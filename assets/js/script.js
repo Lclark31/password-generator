953a6b099084ef71d046b060c3ec96fe975b569b
Lashaun <clarklas831@gmail.com>
2021-06-17 23:26:34 -0500

    functional

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,5 @@
 // Assignment code here
 var chars = `0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz!@#$%^&*_-+=`
-var chosenSet = ``
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
@@ -9,13 +8,15 @@ var generateBtn = document.querySelector("#generate");
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
-
+  
   passwordText.value = password;
-
+  console.log(password.length);
 }
 
 function generatePassword() {
-  console.log(chosenSet)
+  var chosenSet = ``;
+  var pw = ``
+
   let characterLengthPrompt = parseInt(window.prompt(`How many characters would you like your password to be (min: 8 max: 128)?`));
   if (!characterLengthPrompt || typeof characterLengthPrompt === `string`) {
     window.alert(`Please enter a number.`);
@@ -33,36 +34,31 @@ function generatePassword() {
   } 
   let numberPrompt = window.confirm(`Would you like your password to include numbers?`);
   if (numberPrompt) {
-    chosenSet += chars.slice(0, 9);
-    console.log(chosenSet);
+    chosenSet += chars.slice(0, 10);
   }
   let lowerCaseConfirm = window.confirm(`Would you like to include lower case letters?`);
   if (lowerCaseConfirm) {
-    chosenSet += chars.slice(36, 61);
+    chosenSet += chars.slice(36, 62);
   }
   let upperCaseConfirm = window.confirm(`Would you like to include upper case letters?`);
   if (upperCaseConfirm) {
-    chosenSet += chars.slice(10, 35);
+    chosenSet += chars.slice(10, 36);
   }
   let specialCharacterConfirm = window.confirm(`Would you like to include special characters?`);
   if (specialCharacterConfirm) {
-    chosenSet += chars.slice(62, 73);
+    chosenSet += chars.slice(62, 74);
   }
   if (!numberPrompt && !lowerCaseConfirm && !upperCaseConfirm && !specialCharacterConfirm) {
     window.alert(`You must choose at least one character type!`);
     return generatePassword();
   }
-  for (let i=0; i < characterLengthPrompt; i++) {
+  for (var i=0; i < characterLengthPrompt; i++) {
     var randomNum = Math.floor(Math.random() * chars.length);
-    console.log(randomNum);
+    pw += chosenSet.substring(randomNum, randomNum+1);
   }
-
+  return pw;
+  
 };
 
 // Add event listener to generate button
-// for (let len = 0; len < characterLengthPrompt; len++) {
-// pw = Math.floor(Math.random() * pwcharacters.length);
-// password += pwcharacters.substring(pw,pw+1);
-//     console.log(password); }
-    
 generateBtn.addEventListener("click", writePassword);
